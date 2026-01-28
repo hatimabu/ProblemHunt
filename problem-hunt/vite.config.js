d0f04141db19e7836bf814c05cad5d876f951eca
hatimabu <hatimabu@icloud.com>
2026-01-28 07:03:22 -0500

    Updated the Vite config

--- problem-hunt/vite.config.js
+++ problem-hunt/vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, './dist'),
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index-react.html'),
+    },
   },
   plugins: [
     react(),
