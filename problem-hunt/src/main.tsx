a4503752b7d016b87697465c5eb07bb58f744c67
hatimabu <hatimabu@icloud.com>
2026-01-31 07:30:55 -0500

    added sentry CLI for troubleshooting

--- problem-hunt/src/main.tsx
+++ problem-hunt/src/main.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./app/App";
 import "./app/index.css";
+
+Sentry.init({
+  dsn: "https://3f7eb3852a69f9d4bd3bd07a79d5a6d6@o4510805016313856.ingest.us.sentry.io/4510805021032448",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
