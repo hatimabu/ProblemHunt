b0d841d5e2a02dfdff9bf06f10d4fe693409889c
hatimabu <hatimabu@icloud.com>
2026-02-11 10:53:29 -0500

    adding the adding the helmet middleware to set protective HTTP headers

--- problem-hunt/server.js
+++ problem-hunt/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
@@ -11,6 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.use(helmet());
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from dist
